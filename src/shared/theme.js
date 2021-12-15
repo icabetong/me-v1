d8222c6c2d0d13a54df22135c1584c495c4ed442
Isaiah Collins Abetong <isaiahcollins_02@live.com>
2021-12-15 15:34:46 +0800

    removed unused packages

--- src/shared/theme.js
+++ src/shared/theme.js
@@ -5,13 +5,6 @@ const theme = extendTheme({
     initialColorMode: 'dark',
     useSystemColorMode: false,
   },
-  colors: {
-    brand: {
-      900: '#1a365d',
-      800: '#153e75',
-      700: '#2a69ac',
-    },
-  },
   fonts: {
     heading: 'Jetbrains Mono',
     body: 'Jetbrains Mono'
